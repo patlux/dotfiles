9adc86674a8717034306f21962882f9b87617681
Patrick Wozniak <email@patwoz.de>
2016-04-08 00:01:49 +0200

    uupdate lemonbar

--- config/lemonbar/config.sh
+++ config/lemonbar/config.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-DIRNAME=$(dirname $0)
+DIRNAME="$HOME/.config/lemonbar/"
+source "$DIRNAME/siji-icons.sh"
+
+PANEL_FIFO='/tmp/panel-fifo'
+PANEL_PID='/tmp/panel-pid'
+CMD_IFS='󠄳'
 
 # read display resolution
-read RES_X RES_Y <<<$(xdpyinfo | awk -F'[ x]+' '/dimensions:/{print $3, $4}')
+read RES_X RES_Y <<< $(xdpyinfo | awk -F'[ x]+' '/dimensions:/{print $3, $4}')
 
 BAR_HEIGHT=22
 BAR_OFFSET_X=0
@@ -13,9 +18,10 @@ COLOR_BRAND="#0088cc"
 
 BAR_UNDERLINE_COLOR="#404552"
 BAR_UNDERLINE_WIDTH=1
-BAR_BACKGROUND_COLOR="#002d2d2d"
+BAR_BACKGROUND_COLOR="#BB2d2d2d"
 BAR_FOREGROUND_COLOR="#ffffff"
 BAR_FONT="-*-profont-medium-*-*-*-12-*-*-*-*-*-*-*"
 BAR_FONT_ICON="-wuncon-siji-medium-r-normal--10-100-75-75-c-80-iso10646-1"
 
 ICON_COLOR="$COLOR_BRAND"
+
--- config/lemonbar/output.sh
+++ config/lemonbar/output.sh
@@ -1,157 +1,37 @@
 #!/bin/bash
 
-source "$(dirname $0)/siji-icons.sh"
-source "$(dirname $0)/config.sh"
+source "$HOME/.config/lemonbar/config.sh"
 
 
 # ---------------------------------------------------------------------------- #
-# DATETIME
+# FIFO
 
-getDateTime() {
-  echo -n $(date "+%a %b %d, %T")
-}
-
-DateTime() {
-  local block="%{B$BAR_UNDERLINE_COLOR}" # set background color
-  block+=" $ICON_CLOCK_ROUND_OUTLINED"   # clock icon
-  block+=" $(getDateTime)"               # set date
-  block+=" %{B-}"                        # reset background color
-  echo "$block"
-}
+[ -e "$PANEL_FIFO" ] && rm "$PANEL_FIFO"
+mkfifo "$PANEL_FIFO"
 
 
 # ---------------------------------------------------------------------------- #
-# VOLUME
+# PID
 
-getVolumeState() {
-  volumeState=$(awk -F"[][]" '/dB/ { print $6 }' <(amixer -M sget Master))
-  echo "$volumeState"
-}
-
-getVolume() {
-  volume=$(awk -F"[%[]" '/dB/ { print $2 }' <(amixer -M sget Master))
-  echo "$volume"
-}
-
-Volume() {
-  local icon=""
-  local volumeState="$(getVolumeState)"
-  local volume=""
-
-  if [ $volumeState = "on" ]; then
-    volume="$(getVolume)"
-    icon="$ICON_VOLUME"
-
-    if [ $volume -eq 0 ]; then
-      icon="$ICON_VOLUME_ZERO"
-    fi
-
-    volume="$volume%"
-
-  else
-
-    volume="0%"
-    icon="%{F#FF0000}$ICON_VOLUME_MUTE%{F-}"
-
-  fi
-
-  echo "$icon $volume"
-}
+[ -e "$PANEL_PID" ] && pkill -P $(cat "$PANEL_PID")
+echo $$ >"$PANEL_PID"
 
 
 # ---------------------------------------------------------------------------- #
-# RAM
+# INITIALIZE BLOCKS
 
-Ram() {
-  local icon="$ICON_RAM" # set icon
-  local color="#FFFFFF"
-
-  RAM="$(grep -P 'MemTotal|MemFree' /proc/meminfo | awk '{print $2}')"
-  RAM_TOTAL="$(echo $RAM | awk -F ' ' '{ print $1 }' )"
-  RAM_FREE="$(echo $RAM | awk -F ' ' '{ print $2 }' )"
-
-  RAM_CURRENT="$(echo $RAM_TOTAL - $RAM_FREE | bc)"
-
-  RAM_USAGE=" $(echo "scale = 2; $RAM_CURRENT/$RAM_TOTAL*100" | bc)"
-  RAM_USAGE="$(printf "%.0f" "$RAM_USAGE")"
-
-  if [ $RAM_USAGE -gt 90 ]; then
-    color="#FF0000"
-  elif [ $RAM_USAGE -gt 80 ]; then
-    color="#FF7800"
-  fi
-
-  echo "$icon %{F$color}$RAM_USAGE%%{F-}"
-}
+declare -A blocks
+for block in $DIRNAME/blocks/*.sh ; do
+  $block "$@" > "$PANEL_FIFO" &
+done
 
 
 # ---------------------------------------------------------------------------- #
-# ACTIVE WINDOW
+# MAIN LOOP
 
-getActiveWindow() {
-  activeWindowTitle="$(xtitle `xdotool getactivewindow`)"
-  echo "$activeWindowTitle"
-}
-
-ActiveWindowTitle() {
-  local block="$(getActiveWindow)"
-  echo "$block"
-}
-
-
-# ---------------------------------------------------------------------------- #
-# SYSINFO
-
-SysInfos() {
-  items="Vpn Ram Volume"
-  sysinfos=""
-
-  for blockF in $items; do
-    sysinfos+="$($blockF)  "
-  done
-
-  echo "$sysinfos"
-}
-
-
-# ---------------------------------------------------------------------------- #
-# VPN
-
-Vpn() {
-  local color="#FFFFFF"
-  local icon="$ICON_VPN"
-  local vpn_name="$(systemctl list-units | grep ovpn@ | awk '{ print $1 }' | sed -nr 's/.*@([^\w]{2,4})\.service/\1/p')"
-
-  if [ -z "$vpn_name" ]; then
-    color="#FF0000"
-    vpn_name="DIRECT"
-  else
-    color="#00FF00"
-  fi
-
-  echo "%{F$color}$icon $vpn_name%{F-}"
-}
-
-
-# ---------------------------------------------------------------------------- #
-# KERNEL RELEASE
-
-getKernelRelease() {
-  kernelRelease="$(uname -r)"
-  echo "$kernelRelease"
-}
-
-Arch() {
-  local icon="$ICON_ARCH"
-  local block="$(getKernelRelease)"
-  echo "$icon $block"
-}
-
-
-# ---------------------------------------------------------------------------- #
-# OUTPUT
-
-while true; do
+while IFS='' read -r cmd; do
+  IFS="$CMD_IFS" read -ra infos <<< "$cmd"
+  blocks[${infos[0]}]="${infos[1]}"
 
   # INITIALIZE OUTPUT
 
@@ -162,28 +42,43 @@ while true; do
   # LEFT
 
   output+="%{l}"
-  output+=" $(Arch)"
+  output+=" ${blocks[kernel]}"
+  output+=" ${blocks[pacupdates]}"
 
   # -------------------------------------------------------------------------- #
   # CENTER
 
   output+="%{c}"
-  output+="$(ActiveWindowTitle)"
+  output+="${blocks[weather]}"
 
 
   # -------------------------------------------------------------------------- #
   # RIGHT
 
   output+="%{r}"
-  output+="$(SysInfos)"
-  output+="$(DateTime)"
+  # output+="$(SysInfos)"
+  output+="${blocks[vpn]}"
+  output+="  ${blocks[cpu]}"
+  output+="  ${blocks[ram]}"
+  output+="  ${blocks[volume]}"
+  output+="  ${blocks[clock]}"
 
 
   # -------------------------------------------------------------------------- #
   # PIPE TO LEMONBAR
 
   echo -e "$output"
-  sleep 1 # update interval
-done
 
+done < "$PANEL_FIFO" \
+  | lemonbar \
+    -g "$RES_X"x"$BAR_HEIGHT"+"$BAR_OFFSET_X"+"$BAR_OFFSET_X" \
+    -B "$BAR_BACKGROUND_COLOR" \
+    -F "$BAR_FOREGROUND_COLOR" \
+    -f "$BAR_FONT_ICON" \
+    -f "$BAR_FONT" \
+    -u $BAR_UNDERLINE_WIDTH \
+    -U "$BAR_UNDERLINE_COLOR" \
+    -n "dock" \
+    -p
 
+wait
