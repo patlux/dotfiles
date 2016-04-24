75eb40b4d8f595369cb9226aa05b47e643b74f0e
Patrick Wozniak <email@patwoz.de>
2016-04-24 21:31:58 +0200

    rvm path commented

--- home/rvm/.bash/rvm.sh
+++ home/rvm/.bash/rvm.sh
@@ -4,7 +4,7 @@
 
 rvm_dir="$HOME/.rvm"
 if [ -d "$rvm_dir" ]; then
-  export PATH="$PATH:$rvm_dir/bin" # Add RVM to PATH for scripting
+  # export PATH="$PATH:$rvm_dir/bin" # Add RVM to PATH for scripting
   [[ -s "$rvm_dir/scripts/rvm" ]] && source "$rvm_dir/scripts/rvm"
   [[ -r "$HOME/.rvm/scripts/completion" ]] && source "$HOME/.rvm/scripts/completion"
   export rvm_ignore_gemrc_issues=1 # ignore warning /etc/gemrc
