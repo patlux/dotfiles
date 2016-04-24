b7efb4929e62ca7152f714fb9b312a876b1c2b35
Patrick Wozniak <email@patwoz.de>
2016-04-24 19:51:39 +0200

    replaced nvm with n

---
+++ home/nodejs/.bash/nodejs.sh
@@ -0,0 +1,15 @@
+# ---------------------------------------------------------------------------- #
+#   #NVM
+# ---------------------------------------------------------------------------- #
+
+alias nr='npm run'
+
+export N_PREFIX=$HOME/.n
+export PATH="$N_PREFIX/bin:${PATH}"
+
+# nvm_dir="/usr/share/nvm/"
+
+# if [ -d "$nvm_dir" ]; then
+#   export NVM_SYMLINK_CURRENT=true # create 'current'-symlink to current version
+#   source "$nvm_dir/init-nvm.sh"
+# fi
