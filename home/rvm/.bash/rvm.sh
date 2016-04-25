565aeafed38f26093d4e1c694285cff7a83c6204
Patrick Wozniak <patrick.wozniak@hiq5.com>
2016-04-25 11:20:00 +0200

    rvm aliases

--- home/rvm/.bash/rvm.sh
+++ home/rvm/.bash/rvm.sh
@@ -2,10 +2,17 @@
 #   #RVM
 # ---------------------------------------------------------------------------- #
 
+load_aliases() {
+  alias mux='tmuxinator'
+}
+
+
 rvm_dir="$HOME/.rvm"
 if [ -d "$rvm_dir" ]; then
   # export PATH="$PATH:$rvm_dir/bin" # Add RVM to PATH for scripting
   [[ -s "$rvm_dir/scripts/rvm" ]] && source "$rvm_dir/scripts/rvm"
   [[ -r "$HOME/.rvm/scripts/completion" ]] && source "$HOME/.rvm/scripts/completion"
   export rvm_ignore_gemrc_issues=1 # ignore warning /etc/gemrc
+
+  load_aliases
 fi
