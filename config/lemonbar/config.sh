572c91052d1cdac5c5336ee0123214b9168957a4
Patrick Wozniak <patrick.wozniak@hiq5.com>
2016-11-11 12:24:53 +0100

    hidpi resolution at office

--- config/lemonbar/config.sh
+++ config/lemonbar/config.sh
@@ -10,7 +10,7 @@ CMD_IFS='󠄳'
 # read display resolution
 read RES_X RES_Y <<< $(xdpyinfo | awk -F'[ x]+' '/dimensions:/{print $3, $4}')
 
-BAR_HEIGHT=18
+BAR_HEIGHT=24
 BAR_OFFSET_X=0
 BAR_OFFSET_Y=0
 
@@ -20,8 +20,7 @@ BAR_UNDERLINE_COLOR="#5294e2"
 BAR_UNDERLINE_WIDTH=1
 BAR_BACKGROUND_COLOR="#BB3E424D"
 BAR_FOREGROUND_COLOR="#ffffff"
-BAR_FONT="-*-profont-medium-*-*-*-12-*-*-*-*-*-*-*"
+BAR_FONT="-*-profont-medium-*-*-*-14-*-*-*-*-*-*-*"
 BAR_FONT_ICON="-wuncon-siji-medium-r-normal--10-100-75-75-c-80-iso10646-1"
 
 ICON_COLOR="$COLOR_BRAND"
-
