7455078b51dcad0d4edbd675e9001b03ea3ed11b
Patrick Wozniak <email@patwoz.de>
2023-02-03 07:32:55 +0100

    update configs

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1,94 +1,120 @@
 lvim.colorscheme = "sonokai"
 
-lvim.leader = "space"
-lvim.keys.visual_mode["Y"] = '"*y'
-
 lvim.format_on_save = true
 lvim.lint_on_save = true
 
-lvim.builtin.notify.active = true
+-- lvim.builtin.notify.active = true
+
+lvim.lsp.installer.setup.automatic_installation = true
+
+-- --------------
+-- KEYMAPPINGS ---
+
+lvim.leader = "space"
+
+lvim.keys.visual_mode["Y"] = '"*y'
+
+lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
+lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 
 -- --------------
 -- TREESITTER ---
 
 lvim.builtin.treesitter.ensure_installed = {
-	"bash",
-	"css",
-	"dockerfile",
-	"go",
-	"graphql",
-	"html",
-	"javascript",
-	"jsdoc",
-	"json",
-	"json5",
-	"lua",
-	"nix",
-	"python",
-	"regex",
-	"rust",
-	"scss",
-	"toml",
-	"tsx",
-	"typescript",
-	"vim",
-	"yaml"
+  "bash",
+  "css",
+  "dockerfile",
+  "go",
+  "graphql",
+  "html",
+  "javascript",
+  "jsdoc",
+  "json",
+  "json5",
+  "kotlin",
+  "lua",
+  "nix",
+  "python",
+  "regex",
+  "rust",
+  "scss",
+  "toml",
+  "tsx",
+  "typescript",
+  "vim",
+  "yaml",
+  "zig"
 }
 lvim.builtin.treesitter.highlight.enabled = true
 
 -- -------
 -- LSP ---
 
+local with_root_file = function(builtin, file)
+  return builtin.with {
+    condition = function(utils)
+      return utils.root_has_file(file)
+    end,
+  }
+end
+
+local nls = require "null-ls"
+local b = nls.builtins
+
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
-  {
-    exe = "prettier",
-    ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
-    filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact", "css", "json", "yaml" },
-  },
+  -- {
+  --   exe = "prettier",
+  --   filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact", "css", "json", "yaml" },
+  --   root_dir = require("null-ls.utils").root_pattern(".prettierrc")
+  -- },
+  b.formatting.prettier
+  -- with_root_file(b.formatting.prettier, { "node_modules/.bin/prettier" }),
+  -- with_root_file(b.formatting.rome.with { filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact",
+  --   "css", "json", "yaml" } }, { "node_modules/.bin/rome" }),
+  -- with_root_file(b.formatting.gofmt, { "go.mod" })
 }
 
--- local linters = require "lvim.lsp.null-ls.linters"
--- linters.setup {
---   {
---     exe = "eslint",
---     filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact", "css", "json", "yaml" },
---   },
--- }
-
--- https://www.lunarvim.org/languages/#multi-languages-per-formatter
--- https://github.com/LunarVim/LunarVim/issues/1705
--- lvim.lsp.on_attach_callback = function(client, _)
---   if client.name == "tsserver" or client.name == "jsonls" then
---     client.resolved_capabilities.document_formatting = false
---     client.resolved_capabilities.document_range_formatting = false
---   end
--- end
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup {
+  with_root_file(b.code_actions.eslint, { ".eslintrc" }),
+  with_root_file(b.diagnostics.golangci_lint, { "go.mod" }),
+}
 
 -- --------
 -- Nvimtree
 
 lvim.builtin.nvimtree.side = "left"
-lvim.builtin.nvimtree.show_icons.git = 1
+-- lvim.builtin.nvimtree.show_icons.git = 1
 lvim.builtin.nvimtree.hide_dotfiles = 0
 
 -- -------
 -- PLUGINS
 
 lvim.plugins = {
-  {"sainnhe/sonokai"},
- 	{
+  { "sainnhe/sonokai" },
+  {
     "norcalli/nvim-colorizer.lua",
     config = function()
       require("colorizer").setup()
     end,
   },
-  {"APZelos/blamer.nvim"},
+  { "APZelos/blamer.nvim" },
   {
     "ray-x/lsp_signature.nvim",
-    config = function() require"lsp_signature".on_attach() end,
+    config = function() require "lsp_signature".on_attach() end,
     event = "BufRead"
-  }
+  },
+  { "isobit/vim-caddyfile" }
+}
+
+-- -------
+-- TELESCOPE
+
+lvim.builtin.telescope.defaults.file_ignore_patterns = {
+  "%.app/",
+  "build/",
+  "dist/"
 }
 
 -- -------
@@ -97,3 +123,6 @@ lvim.plugins = {
 vim.cmd("au BufRead,BufNewFile Appfile set filetype=ruby")
 vim.cmd("au BufRead,BufNewFile Fastfile set filetype=ruby")
 vim.cmd("au BufRead,BufNewFile Matchfile set filetype=ruby")
+vim.cmd("au BufRead,BufNewFile *.gradle set syntax=groovy")
+vim.cmd("au BufRead,BufNewFile *.zig set ft=zig")
+vim.cmd("au BufRead,BufNewFile *.kt set syntax=kotlin")
--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -3,8 +3,7 @@
 let
   name = "patwoz";
 in
-{
-  programs.home-manager.enable = true;
+{ programs.home-manager.enable = true;
 
   home.username = name;
   home.homeDirectory = "/Users/${name}";
@@ -12,29 +11,32 @@ in
   home.stateVersion = "21.11";
 
   home.packages = [
-   # pkgs.htop
-   pkgs.pinentry_curses
-   # pkgs.alacritty
-   # https://giters.com/LnL7/nix-darwin/issues/362
-   # pkgs.kitty
-   pkgs.entr # https://eradman.com/entrproject/
-   # pkgs.nodejs-14_x
-   pkgs.ripgrep
-   pkgs.fd
-   pkgs.delta
-   pkgs.ruby
-   pkgs.ncdu
-   # pkgs.scrcpy
-   pkgs.watchman
-   pkgs.translate-shell
-   pkgs.golangci-lint
-   pkgs.imagemagick
-   pkgs.awscli2
-   pkgs.ffmpeg
-   # pkgs.helix # https://github.com/helix-editor/helix
-   pkgs.gh # GitHub Client
-   pkgs.rsync
-   # pkgs.dprint # https://dprint.dev
+    # pkgs.htop
+    pkgs.pinentry_curses
+    # pkgs.alacritty
+    # https://giters.com/LnL7/nix-darwin/issues/362
+    # pkgs.kitty
+    pkgs.entr # https://eradman.com/entrproject/
+    # pkgs.nodejs-14_x
+    pkgs.ripgrep
+    pkgs.fd
+    pkgs.delta
+    # pkgs.ruby
+    pkgs.ncdu
+    # pkgs.scrcpy
+    pkgs.watchman
+    pkgs.translate-shell
+    # pkgs.golangci-lint
+    pkgs.imagemagick
+    pkgs.awscli2
+    pkgs.ffmpeg
+    # pkgs.helix # https://github.com/helix-editor/helix
+    pkgs.gh # GitHub Client
+    pkgs.rsync
+    # pkgs.dprint # https://dprint.dev
+    pkgs.rustc
+    pkgs.cargo
+    pkgs.hyperfine
   ];
 
   home.sessionVariables = {
@@ -48,13 +50,14 @@ in
     "/opt/homebrew/sbin"
     "${config.home.homeDirectory}/.local/bin"
     "${config.home.homeDirectory}/.cargo/bin"
-    "${config.home.homeDirectory}/.golang/bin"
+    # "${config.home.homeDirectory}/go/bin"
     "${config.home.homeDirectory}/.n/bin"
     "${config.home.homeDirectory}/.bin"
+    "${config.home.homeDirectory}/.bin/bin"
   ];
 
   home.file.".gemrc".text = ''
-gem: --user-install --env-shebang --no-document
+    gem: --user-install --env-shebang --no-document
   '';
 
   programs.zsh = {
@@ -66,33 +69,45 @@ gem: --user-install --env-shebang --no-document
     history = {
       size = 10000;
       path = "${config.xdg.dataHome}/zsh/history";
-    }; 
+    };
 
     prezto = {
       enable = true;
       editor = { keymap = "vi"; };
       pmodules = [ "environment" "terminal" "editor" "history" "directory" "spectrum" "utility" "completion" "history-substring-search" "prompt" "git" ];
     };
- 
-     envExtra = "
+
+    envExtra = "
  export N_PREFIX=\"${config.home.homeDirectory}/.n\"
      ";
- 
-     initExtra = "
+
+    initExtra = "
+ plugins=(
+  asdf
+ )
+
+ # asdf (https://asdf-vm.com/guide/getting-started.html#_3-install-asdf)
+ . $HOME/.asdf/asdf.sh
+
+
  # https://github.com/bitwarden/cli/blob/8b650666c593efa19ee54ef7360321de63efe0e2/src/bw.ts#L106
  export BITWARDENCLI_APPDATA_DIR=$HOME/.config/bitwarden-cli
  export GEM_HOME=$(ruby -e \"puts Gem.user_dir\")
  export PATH=$PATH:$GEM_HOME/bin
- 
- alias dgit='git --git-dir ~/.dotfiles/.git --work-tree=$HOME'
- ls = \"exa\";
- ll = \"exa -l\";
- la = \"exa -a\";
- lt = \"exa --tree\";
- lla = \"exa -la\";
 
- export VOLTA_HOME=\"$HOME/.volta\"
- export PATH=\"$VOLTA_HOME/bin:$PATH\"
+ export GOPATH=$(go env GOPATH)
+ export GOROOT=$(go env GOROOT)
+ export GOBIN=$(go env GOBIN)
+ export PATH=$PATH:$GOPATH/bin
+ export PATH=$PATH:$GOROOT/bin
+ export PATH=$PATH:$GOBIN
+ 
+ alias dgit=\"git --git-dir ~/.dotfiles/.git --work-tree=$HOME\"
+ alias ls=\"exa\"
+ alias ll=\"exa -l\"
+ alias la=\"exa -a\"
+ alias lt=\"exa --tree\"
+ alias lla=\"exa -la\"
  
  GPG_TTY=\"$(tty)\"
  export GPG_TTY
@@ -107,51 +122,67 @@ gem: --user-install --env-shebang --no-document
    java -version
  }
  
- export ANDROID_SDK_ROOT=/opt/homebrew/Caskroom/android-sdk/4333796
- # export PATH=/opt/homebrew/Caskroom/android-sdk/4333796/tools:$PATH
- # export PATH=/opt/homebrew/Caskroom/android-platform-tools/32.0.0/platform-tools:$PATH
+ export ANDROID_SDK_ROOT=$HOME/Library/Android/sdk
+ export PATH=$ANDROID_SDK_ROOT/tools:$PATH
+ export PATH=$ANDROID_SDK_ROOT/platform-tools:$PATH
+
+ export PATH=$HOME/Library/Python/3.8/bin:$PATH
+ export PATH=$HOME/.bun/bin:$PATH
+
+ # https://github.com/oven-sh/bun#install-zig-macos
+ # export PATH=\"$(brew --prefix llvm@13)/bin:$HOME/.bun-tools/zig:$PATH\"
+ # export LDFLAGS=\"$LDFLAGS -L$(brew --prefix llvm@13)/lib\"
+ # export CPPFLAGS=\"$CPPFLAGS -I$(brew --prefix llvm@13)/include\"
+ export PATH=\"$HOME/.bun-tools/zig:$PATH\"
+
+  [ -s \"/Users/patwoz/.bun/_bun\" ] && source \"/Users/patwoz/.bun/_bun\"
  
  if [ -f ~/.zshrc_secret ]; then
      source ~/.zshrc_secret
  fi
+
+ if [ -f ~/.gvm/scripts/gvm ]; then
+    source ~/.gvm/scripts/gvm
+ fi
+
      ";
-   };
+  };
 
- programs.fzf.enable = true;
- programs.fzf.defaultCommand = "fd --type file --hidden --exclude .git";
+  programs.fzf.enable = true;
+  programs.fzf.defaultCommand = "fd --type file --hidden --exclude .git";
 
- # takes to long to install due compiling
- # programs.exa.enable = true;
- # programs.exa.enableAliases = true;
+  # takes to long to install due compiling
+  # programs.exa.enable = true;
+  # programs.exa.enableAliases = true;
 
- programs.bat.enable = true;
- programs.go.enable = true;
- programs.jq.enable = true;
-#
- programs.git = {
-   enable = true;
-   userName = "Patrick Wozniak";
-   userEmail = "email@patwoz.de";
-   signing.key = "0D4DE3BE5B9D660B";
-   signing.signByDefault = true;
-   delta = { enable = true; };
-   aliases = { co = "checkout"; ci = "commit"; br = "branch"; pushf = "push --force-with-lease"; };
-   includes = [
-     {
-       condition = "gitdir:~/dev/pdg";
-     	contents = {
-         user = {
-           email = "patrick.wozniak.extern@porsche.digital";
-           name = "Patrick Wozniak";
-           signingKey = "B33CA176B9EF9ECFE39CFCEC1A32081F816CE44F";
-         };
-         commit = {
-           gpgSign = true;
-         };
-};
-     }
-   ];
- };
+  programs.bat.enable = true;
+  # programs.go.enable = true;
+  programs.jq.enable = true;
+  #
+  programs.git = {
+    enable = true;
+    userName = "Patrick Wozniak";
+    userEmail = "email@patwoz.de";
+    signing.key = "0D4DE3BE5B9D660B";
+    signing.signByDefault = true;
+    delta = { enable = true; };
+    aliases = { co = "checkout"; ci = "commit"; br = "branch"; pushf = "push --force-with-lease"; };
+    includes = [
+      {
+        condition = "gitdir:~/dev/pdg";
+        contents = {
+          user = {
+            email = "patrick.wozniak.extern@porsche.digital";
+            name = "Patrick Wozniak";
+            signingKey = "B33CA176B9EF9ECFE39CFCEC1A32081F816CE44F";
+          };
+          commit = {
+            gpgSign = true;
+          };
+        };
+      }
+    ];
+  };
 
   # programs.neovim = {
   #   enable = true;
@@ -190,24 +221,24 @@ gem: --user-install --env-shebang --no-document
     };
   };
 
-#   home.file.".gnupg/gpg-agent.conf".text = ''
-# default-cache-ttl 600
-# max-cache-ttl 7200
+  #   home.file.".gnupg/gpg-agent.conf".text = ''
+  # default-cache-ttl 600
+  # max-cache-ttl 7200
 
-# pinentry-program ${config.home.homeDirectory}/.nix-profile/bin/pinentry-curses
-#   '';
+  # pinentry-program ${config.home.homeDirectory}/.nix-profile/bin/pinentry-curses
+  #   '';
 
-#   services.gpg-agent = {
-#     enable = true;
-#     defaultCacheTtl = 600;
-#     maxCacheTtl = 7200;
-#     pinentryFlavor = "curses";
-#   };
-  
-#   programs.vscode = {
-#     enable = true;
-#     userSettings = {
-#       "update.channel" = "none";
-#     };
-#   };
+  #   services.gpg-agent = {
+  #     enable = true;
+  #     defaultCacheTtl = 600;
+  #     maxCacheTtl = 7200;
+  #     pinentryFlavor = "curses";
+  #   };
+
+  #   programs.vscode = {
+  #     enable = true;
+  #     userSettings = {
+  #       "update.channel" = "none";
+  #     };
+  #   };
 }
