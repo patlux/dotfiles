f3a58787d9719140bd7c0d1e0bf32e21b545ec19
Patrick Wozniak <email@patwoz.de>
2024-09-06 08:58:01 +0200

    lvim: update config

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -153,6 +153,28 @@ lvim.plugins = {
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
     opts = {},
   },
+  -- {
+  --   "yetone/avante.nvim",
+  --   event = "VeryLazy",
+  --   build = "make", -- This is Optional, only if you want to use tiktoken_core to calculate tokens count
+  --   opts = {
+  --     -- add any opts here
+  --   },
+  --   dependencies = {
+  --     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+  --     "stevearc/dressing.nvim",
+  --     "nvim-lua/plenary.nvim",
+  --     "MunifTanjim/nui.nvim",
+  --     --- The below is optional, make sure to setup it properly if you have lazy=true
+  --     {
+  --       'MeanderingProgrammer/render-markdown.nvim',
+  --       opts = {
+  --         file_types = { "markdown", "Avante" },
+  --       },
+  --       ft = { "markdown", "Avante" },
+  --     },
+  --   },
+  -- }
 }
 
 local api = require("typescript-tools.api")
